2d27280d33fce23cbe9b23b8837d147c237db0c9
warmoil <jipmj12@gmail.com>
2024-09-18 20:58:39 +0900

    Initial commit

---
+++ src/main/kotlin/com/aps/cafe/CafeApplication.kt
@@ -0,0 +1,11 @@
+package com.aps.cafe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CafeApplication
+
+fun main(args: Array<String>) {
+	runApplication<CafeApplication>(*args)
+}
