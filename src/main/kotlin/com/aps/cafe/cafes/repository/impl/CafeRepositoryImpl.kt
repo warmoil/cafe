b51235c1cf1a73cc0efc4dc12460b138bfe1eee9
warmoil <jipmj12@gmail.com>
2024-09-23 03:24:02 +0900

    cafeRepository 테스트 코드작성
    #10

--- src/main/kotlin/com/aps/cafe/cafes/repository/impl/CafeRepositoryImpl.kt
+++ src/main/kotlin/com/aps/cafe/cafes/repository/impl/CafeRepositoryImpl.kt
@@ -1,36 +1,70 @@
 package com.aps.cafe.cafes.repository.impl
 
+import com.aps.cafe.cafes.entity.CafeEntity
 import com.aps.cafe.cafes.model.CafeModel
 import com.aps.cafe.cafes.repository.CafeRepository
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
+interface CafeJPARepository : JpaRepository<CafeEntity, Long> {
+    fun existsByName(name: String): Boolean
+    fun findByName(name: String): CafeEntity?
+}
+
 @Repository
-class CafeRepositoryImpl : CafeRepository {
+class CafeRepositoryImpl(
+    private val cafeRepo: CafeJPARepository
+) : CafeRepository {
+
+
     override fun existsByCafeName(cafeName: String): Boolean {
-        TODO("Not yet implemented")
+        return cafeRepo.existsByName(name = cafeName)
     }
 
     override fun getByCafeName(cafeName: String): CafeModel? {
-        TODO("Not yet implemented")
+        return cafeRepo.findByName(cafeName)?.toModel()
     }
 
     override fun getByCafeId(id: Long): CafeModel? {
-        TODO("Not yet implemented")
+        return cafeRepo.findById(id).orElse(null)?.toModel()
     }
 
     override fun create(model: CafeModel): CafeModel {
-        TODO("Not yet implemented")
+        cafeRepo.save(model.toEntity())
+        return model
     }
 
     override fun update(model: CafeModel): CafeModel {
-        TODO("Not yet implemented")
+        cafeRepo.save(model.toEntity())
+        return model
     }
 
     override fun delete(id: Long) {
-        TODO("Not yet implemented")
+        cafeRepo.deleteById(id)
     }
 
     override fun getAll(): List<CafeModel> {
-        TODO("Not yet implemented")
+        return cafeRepo.findAll().map { it.toModel() }
     }
+
+    private fun CafeEntity.toModel() = CafeModel(
+        id = id,
+        name = name,
+        description = description,
+        ownerId = ownerId,
+        ownerNickname = ownerNickname,
+        createdAt = createdAt,
+        updatedAt = updatedAt,
+    )
+
+    private fun CafeModel.toEntity() = CafeEntity(
+        id = id,
+        name = name,
+        description = description,
+        ownerId = ownerId,
+        ownerNickname = ownerNickname,
+        createdAt = createdAt,
+        updatedAt = updatedAt,
+    )
+
 }
